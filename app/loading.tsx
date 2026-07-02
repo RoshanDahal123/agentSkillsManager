826f77fb681702c563007c73a488112eb8221c26
RoshanDahal123 <dahalroshan765@gmail.com>
2026-07-02 14:10:43 +0545

    feat:handling GlobalLoading

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+export default function GlobalLoading(){
+    return(
+<div className="min-h-screen flex items-center justify-center">
+    <div className="text-center">
+        <span className="loading loading-spinner loading-lg text-primary"></span>
+        <p className="mt-4 text-base-content/70">Loading...</p>
+    </div>
+</div>
+
+    )
+}
